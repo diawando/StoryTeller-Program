45d3f5b1cb44c39a54cdc6a8ee3607f0c2c3241a
diawando <abduldiawade777@gmail.com>
2023-08-25 17:35:14 +0000

    first step of this the program

---
+++ main.py
@@ -0,0 +1,6 @@
+
+
+fullname = input("Hi, Welcome to the story teller program, what's your name please?  ")
+
+print("welcome", fullname, "You will be talking abour your amazing experience in Azubi so far")
+
